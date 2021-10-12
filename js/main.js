780980f10839f3e0df98393f5d427184d7abb744
Alexander <sasha.timofeev19999@mail.ru>
2021-10-12 11:01:08 +0300

    adding-popup

--- js/main.js
+++ js/main.js
@@ -20,4 +20,33 @@ $(document).ready(function(){
         ],
         appendArrows: $('.reviews__arrows'),
     });
+    let burger768Open = $('.navigation__burger-button_open');
+    let burger768Close = $('.navigation__burger-button_close');
+    let navigationPopup = $('.navigation__popup');
+
+    burger768Open[0].addEventListener('click', function () {
+        navigationPopup.removeClass('none');
+        burger768Open.addClass('none');
+        burger768Close.removeClass('none');
+    });
+    burger768Close[0].addEventListener('click', function () {
+        navigationPopup.addClass('none');
+        burger768Close.addClass('none');
+        burger768Open.removeClass('none');
+    });
+
+    let headerBurgerOpen = $('.header__burger_open');
+    let headerBurgerClose = $('.header__burger_close');
+    let headerPopup = $('.header-popup');
+
+    headerBurgerOpen[0].addEventListener('click', function () {
+        headerPopup.removeClass('none');
+        headerBurgerOpen.addClass('none');
+        headerBurgerClose.removeClass('none');
+    });
+    headerBurgerClose[0].addEventListener('click', function () {
+        headerPopup.addClass('none');
+        headerBurgerClose.addClass('none');
+        headerBurgerOpen.removeClass('none');
+    });
 });
